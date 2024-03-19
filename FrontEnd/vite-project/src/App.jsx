623bc71c339d5c2a7556e6d023205e778a86c9d1
Shaunak <ssnagvenkar@gmail.com>
2024-03-19 23:17:19 +0530

    Changes to Routing

---
+++ FrontEnd/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path=''>
+
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
