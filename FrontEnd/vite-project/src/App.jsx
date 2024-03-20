a98bda33393eb012ed23d8a716faeea2eb09f268
Shaunak <ssnagvenkar@gmail.com>
2024-03-20 10:16:06 +0530

    Changes to login

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom'
+import Login from './Page/Login/Login'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
       <Router basename='/login'>
         <Routes>
           <Route path=''>
-
+            <Route index={<Login/>}/>
           </Route>
         </Routes>
       </Router>
--- FrontEnd/vite-project/vite.config.js
+++ FrontEnd/vite-project/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // Using the React plugin instead of Vue
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+});
